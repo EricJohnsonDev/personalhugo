7c1d2b1e820b9c07910c1e02d11cdc38250aae4c
FenderShredder512 <ejmetall@gmail.com>
2019-04-04 18:27:41 -0500

    Content and style updates

---
+++ static/js/themes.js
@@ -0,0 +1,17 @@
+var lightTheme = {
+  mainBackground: "bg-white",
+  mainText: "text-dark",
+  linkText: "text-primary",
+  toggleIcon: "fa-moon"
+};
+
+var darkTheme = {
+  mainBackground: "bg-black",
+  mainText: "text-white",
+  linkText: "text-warning",
+  toggleIcon: "fa-sun"
+};
+
+var isDark = true;
+
+var themeColor = document.querySelector("meta[name=theme-color]");
